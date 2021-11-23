6cc2eb7500a697331fa5cf25b0f5b4f7975852c5
Mohsen <behroozi.mohsen1@gmail.com>
2021-11-23 16:03:44 +0330

    category template

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,18 @@ Route::prefix('admin')->namespace('Admin')->group(function(){
     Route::get('/', 'AdminDashboardController@index')->name('admin.home');
     // Route::get('/', [AdminDashboardController::class])->name('admin.home');
 
+    Route::prefix('market')->namespace('Market')->group(function(){
+
+        //category
+        Route::prefix('category')->group(function(){
+            Route::get('/', 'CategoryController@index')->name('admin.market.category.index');
+            Route::get('/create', 'CategoryController@create')->name('admin.market.category.create');
+            Route::post('/store', 'CategoryController@store')->name('admin.market.category.store');
+            Route::get('/edit/{id}', 'CategoryController@edit')->name('admin.market.category.edit');
+            Route::put('/update/{id}', 'CategoryController@update')->name('admin.market.category.update');
+            Route::delete('/delete/{id}', 'CategoryController@destroy')->name('admin.market.category.destroy');
+        });
+
+    });
+
 });
