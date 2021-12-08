dfc49d61667261f8316e3e5f3728c9e77ea4b6cb
Mohsen <behroozi.mohsen1@gmail.com>
2021-12-08 14:44:50 +0330

    slug

--- app/Http/Controllers/Admin/Content/CategoryController.php
+++ app/Http/Controllers/Admin/Content/CategoryController.php
@@ -42,8 +42,6 @@ class CategoryController extends Controller
     public function store(PostCategoryRequest $request)
     {
         $inputs = $request->all();
-        $inputs['slug'] = str_replace(' ','-', $inputs['name'].'-'. Str::random(5));
-
         $postCategory = PostCategory::create($inputs);
         return redirect()->route('admin.content.category.index')->with('alert-section-success', 'دسته بندی جدید شما با موفقیت ثبت شد');
     }
--- app/Models/Content/PostCategory.php
+++ app/Models/Content/PostCategory.php
@@ -2,14 +2,27 @@
 
 namespace App\Models\Content;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class PostCategory extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory, SoftDeletes, Sluggable;
 
 
     protected $fillable =['name','description', 'slug', 'image', 'status', 'tags'];
+
+
+    public function sluggable(): array
+   {
+
+        return [
+            'slug' => [
+                'source' => 'name'
+            ]
+        ];
+
+    }
 }
